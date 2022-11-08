19fc0ed7fbea0650450a60bf64bb854ec6fe3b18
“Marek <mmpietkiewicz@gmail.com>
2022-11-08 14:53:15 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { FC, useState, useCallback, useEffect } from "react";
+import "./App.css";
+
+const App: FC = () => {
+  return <div className="App">GITHUB USER FINDER :)</div>;
+};
+
+export default App;
